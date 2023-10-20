2ba044f349c9f25ce2f22d119a2a7b7abf966b71
db-roo <iblpkmdd@icloud.com>
2023-10-20 22:54:44 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-const searchEl = document.querySelector('.search');
-const searchinputEl = searchEl.querySelector('input');
-
-searchEl.addEventListener('click', function() {
-  searchinputEl.focus();
-});
-
-searchinputEl.addEventListener('focus', function() {
-  searchEl.classList.add('focused');
-  searchinputEl.setAttribute('placeholder', '통합검색');
-});
-
-searchinputEl.addEventListener('blur', function() {
-  searchEl.classList.remove('focused');
-  searchinputEl.setAttribute('placeholder', '');
-});
-
 // 뱃지함수 등록
 const badgeEl = document.querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
@@ -144,7 +127,3 @@ spyEls.forEach(function(spyEl){
   .setClassToggle(spyEl,'show')
   .addTo(new ScrollMagic.Controller());
 });
-
-
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear();
